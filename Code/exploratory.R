b761484dfc7ae9171b67186cfc389cd61fe50d4e
Gautam Sabnis <gautamsabnis15@gmail.com>
2020-07-14 16:51:31 -0400

    Edited exploratory.R and added plots by genetic background

--- Code/exploratory.R
+++ Code/exploratory.R
@@ -1,4 +1,3 @@
-
 #setwd("/Users/sabnig/Documents/Projects/Komp/Temp")
 #data_per_stride <- read.delim('../Data/kompdf', stringsAsFactors = TRUE)
 
@@ -23,8 +22,8 @@ Strain <- sapply(seq(dim(data_per_animal)[1]), function(x) data_per_stride[data_
 data_per_animal <- cbind(Strain, data_per_animal)
 
 #Pick Strains with atleast 8 animals 
-Strain8 <- names(table(data_per_animal$Strain))[table(data_per_animal$Strain) >= 8]
-data_per_animal <- data_per_animal[data_per_animal$Strain %in% Strain8,]
+#Strain8 <- names(table(data_per_animal$Strain))[table(data_per_animal$Strain) >= 8]
+#data_per_animal <- data_per_animal[data_per_animal$Strain %in% Strain8,]
 
 #Remove outliers 
 invisible(sapply(seq(length(unique(data_per_animal$Strain))), function(s) Map(function(p) {
@@ -38,15 +37,15 @@ invisible(sapply(seq(length(unique(data_per_animal$Strain))), function(s) Map(fu
 data_per_strain <- aggregate(x = data_per_animal[,names(data_per_animal) %in% c(Phenos.lin)], by = data_per_animal[c("Strain")], 
 	FUN = function(x) c(mn = mean(x, na.rm=TRUE), md = median(x,na.rm=TRUE), std = sd(x,na.rm=TRUE)))
 
-komp_desc_plot <- function(phenotype, phenoname, align){
-	data_per_animal$sorted_strain = make_sort_column(data_per_animal, 'Strain', phenotype ,FUN = mean)
-	me <- data_per_strain[data_per_strain$Strain == 'C57BL/6NJ', names(data_per_strain) %in% c(phenotype)][1]
-	std <- data_per_strain[data_per_strain$Strain == 'C57BL/6NJ', names(data_per_strain) %in% c(phenotype)][3]
-	df <- as.data.frame(data_per_strain[,phenotype]) 
-	df <- cbind(Strain = data_per_strain$Strain, df)
+komp_desc_plot <- function(df_animal, df_strain, phenotype, phenoname, align){
+	df_animal$sorted_strain = make_sort_column(df_animal, 'Strain', phenotype ,FUN = mean)
+	me <- df_strain[df_strain$Strain == 'C57BL/6NJ', names(df_strain) %in% c(phenotype)][1]
+	std <- df_strain[df_strain$Strain == 'C57BL/6NJ', names(df_strain) %in% c(phenotype)][3]
+	df <- as.data.frame(df_strain[,phenotype]) 
+	df <- cbind(Strain = df_strain$Strain, df)
 
 	if(align == 'h'){
-		ggplot(data_per_animal, aes_string(x = "sorted_strain", y = phenotype)) + geom_point(alpha = 0.5) + 
+		ggplot(df_animal, aes_string(x = "sorted_strain", y = phenotype)) + geom_point(alpha = 0.5) + 
 		geom_point(data = df, aes(x = Strain, y = df[,"mn"]), color = 'red', size = 2.5) + 
 		geom_errorbar(data = df, aes(x = Strain, df[,"mn"], ymin = df[,"mn"]-df[,"std"],
 		ymax = df[,"mn"]+df[,"std"]), color = 'grey') + geom_hline(yintercept = me - std, 
@@ -55,9 +54,9 @@ komp_desc_plot <- function(phenotype, phenoname, align){
 		theme_bw(base_size = 16) + ggtitle('') + theme(legend.position = 'none') + 
         theme(axis.text.x = element_text(angle = 90, size = 5, vjust = 0.6))
     } else {
-		ggplot(data_per_animal, aes_string(y = "sorted_strain", x = phenotype)) + geom_point(alpha = 0.5) + 
-		geom_point(data = data_per_strain, aes(y = Strain, x = df[,"mn"]), color = 'red', size = 2.5) + 
-		geom_errorbarh(data = data_per_strain, aes(y = Strain, df[,"mn"], xmin = df[,"mn"]-df[,"std"],
+		ggplot(df_animal, aes_string(y = "sorted_strain", x = phenotype)) + geom_point(alpha = 0.5) + 
+		geom_point(data = df, aes(y = Strain, x = df[,"mn"]), color = 'red', size = 2.5) + 
+		geom_errorbarh(data = df, aes(y = Strain, df[,"mn"], xmin = df[,"mn"]-df[,"std"],
 		xmax = df[,"mn"]+df[,"std"]), color = 'grey') + geom_vline(xintercept = me - std, 
 		color = "red", size = 1,alpha = 0.3) +  geom_vline(xintercept = me, color = "red", size = 1,alpha = 0.5) +  
 		geom_vline(xintercept = me + std, color = "red", size = 1,alpha = 0.3) + labs(y = 'Strain', x = paste0(phenoname)) +  
@@ -66,14 +65,8 @@ komp_desc_plot <- function(phenotype, phenoname, align){
 	}
 }
 
-p1 <- komp_desc_plot('step_length1', 'Step Length', align='h')
-p2 <- komp_desc_plot('stride_length', 'Stride Length', align='h')
-p3 <- komp_desc_plot('step_width', 'Step Width', align='h')
-p4 <- komp_desc_plot('base_tail_lateral_displacement', 'Base Tail LD', align='h')
-p5 <- komp_desc_plot('tip_tail_lateral_displacement', 'Tip Tail LD', align='h')
-p6 <- komp_desc_plot('nose_lateral_displacement', 'Nose LD', align='h')
-p7 <- komp_desc_plot('limb_duty_factor', 'Limb Duty', align='h')
-p8 <- komp_desc_plot('temporal_symmetry', 'Temporal Symmetry', align='h')
+lapply(seq(length(Phenos.lin)), function(x) assign(paste0("p",x), komp_desc_plot(data_per_animal, 
+	data_per_strain, Phenos.lin[x], Phenos.lin.Nomen[x], align='h'), inherits=TRUE))
 
 plot_grid(p1,p2,p3,nrow=3)
 ggsave(snakemake@output[[1]], width=9, height=12)
@@ -81,4 +74,59 @@ plot_grid(p4,p5,p6,nrow=3)
 ggsave(snakemake@output[[2]], width=9, height=12)
 plot_grid(p7,p8,nrow=2)
 ggsave(snakemake@output[[3]], width=9, height=12)
+
+
+#Genetic backgrounds: <em1J>,(KOMP),(EUCOMM)
+explore_strain_family <- function(family = c("em1J","KOMP","EUCOMM")){
+	toMatch <- c("C57BL/6NJ",family)
+	matches <- unique(grep(paste(toMatch, collapse = "|"), data_per_stride$Strain, value = TRUE))
+	data_per_stride <- data_per_stride[data_per_stride$Strain %in% matches,]
+	data_per_stride$Strain <- as.factor(data_per_stride$Strain)
+	data_per_stride$Strain <- droplevels(data_per_stride$Strain)
+	data_per_animal <- aggregate(x = data_per_stride[,names(data_per_stride) %in% c(Phenos.lin)], by = data_per_stride[c("MouseID")], FUN = mean)
+	Strain <- sapply(seq(dim(data_per_animal)[1]), function(x) data_per_stride[data_per_stride$MouseID == data_per_animal$MouseID[x], 'Strain'][1])
+	data_per_animal <- cbind(Strain, data_per_animal)
+	data_per_strain <- aggregate(x = data_per_animal[,names(data_per_animal) %in% c(Phenos.lin)], by = data_per_animal[c("Strain")], 
+	FUN = function(x) c(mn = mean(x, na.rm=TRUE), md = median(x,na.rm=TRUE), std = sd(x,na.rm=TRUE)))
+	data_per_strain$Strain <- gsub('.*-(.*)<.*','\\1',data_per_strain$Strain)
+	data_per_animal$Strain <- gsub('.*-(.*)<.*','\\1',data_per_animal$Strain)
+	return(list(data_per_animal,data_per_strain)) 
+}
+
+BGs <- c("em1J","KOMP","EUCOMM")
+invisible(lapply(seq(length(BGs)), function(x) assign(paste0("data_per_strain.",BGs[x]), explore_strain_family(BGs[x])[[2]],
+	inherits=TRUE)))
+invisible(lapply(seq(length(BGs)), function(x) assign(paste0("data_per_animal.",BGs[x]), explore_strain_family(BGs[x])[[1]],
+	inherits=TRUE)))
+
+#em1J
+lapply(seq(length(Phenos.lin)), function(x) assign(paste0("p",x), komp_desc_plot(data_per_animal.em1J, 
+	data_per_strain.em1J, Phenos.lin[x], Phenos.lin.Nomen[x], align='h'), inherits=TRUE))
+plot_grid(p1,p2,p3,nrow=3)
+ggsave(snakemake@output[[4]], width=9, height=12)
+plot_grid(p4,p5,p6,nrow=3)
+ggsave(snakemake@output[[5]], width=9, height=12)
+plot_grid(p7,p8,nrow=2)
+ggsave(snakemake@output[[6]], width=9, height=12)
+
+#KOMP
+lapply(seq(length(Phenos.lin)), function(x) assign(paste0("p",x), komp_desc_plot(data_per_animal.KOMP, 
+	data_per_strain.KOMP, Phenos.lin[x], Phenos.lin.Nomen[x], align='h'), inherits=TRUE))
+plot_grid(p1,p2,p3,nrow=3)
+ggsave(snakemake@output[[7]], width=9, height=12)
+plot_grid(p4,p5,p6,nrow=3)
+ggsave(snakemake@output[[8]], width=9, height=12)
+plot_grid(p7,p8,nrow=2)
+ggsave(snakemake@output[[9]], width=9, height=12)
+
+#EUCOMM
+lapply(seq(length(Phenos.lin)), function(x) assign(paste0("p",x), komp_desc_plot(data_per_animal.EUCOMM, 
+	data_per_strain.EUCOMM, Phenos.lin[x], Phenos.lin.Nomen[x], align='h'), inherits=TRUE))
+plot_grid(p1,p2,p3,nrow=3)
+ggsave(snakemake@output[[10]], width=9, height=12)
+plot_grid(p4,p5,p6,nrow=3)
+ggsave(snakemake@output[[11]], width=9, height=12)
+plot_grid(p7,p8,nrow=2)
+ggsave(snakemake@output[[12]], width=9, height=12)
+
 
